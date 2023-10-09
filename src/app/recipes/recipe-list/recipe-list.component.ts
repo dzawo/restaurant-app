918ebad77e8fe5ffb77f7b30bd09746ee52c2b65
Marshall <marshall@uniclicksoft.co.za>
2023-10-09 08:27:53 +0200

    Comoleted project layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  recipes: Recipe[];
 
+  constructor() {
+    this.recipes = [
+      new Recipe(
+        'Gulash',
+        'For cooking gulash',
+        'https://images.pexels.com/photos/4393021/pexels-photo-4393021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+      ),
+      new Recipe(
+        'Gulash',
+        'For cooking gulash',
+        'https://media.istockphoto.com/id/1060535922/photo/american-chop-suey-american-goulash-with-elbow-pasta-beef-and-tomatoes-close-up-horizontal.jpg?s=612x612&w=0&k=20&c=y87kR4apeU5a9BOtmnDneZv_3okFPlwTRUtpkgI1Yas='
+      ),
+    ];
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingedient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[];
 
+  constructor() {
+    this.ingredients = [
+      new Ingredient('Onions', 3330.04),
+      new Ingredient('Tomatoes', 3330.04),
+    ];
+  }
 }
